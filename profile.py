f7c2d86e880101a21a5b03cbde02e631b294d03c
zachdyszel <145168936+zachdyszel@users.noreply.github.com>
2026-03-19 16:25:41 -0400

    fixed

--- profile.py
+++ profile.py
@@ -3,11 +3,10 @@ import geni.rspec.pg as rspec
 
 request = portal.context.makeRequestRSpec()
 
-node = request.XenVM("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
-node.routable_control_ip = "true"
+node = request.RawPC("node")
 
-# Install Docker instead of Apache
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y docker.io"))
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo systemctl start docker"))
